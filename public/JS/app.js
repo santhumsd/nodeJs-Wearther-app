c2cc2b15a22cde126401c2070211d94e89046fab
santhosh MS <santhoshms25@gmail.com>
2020-12-29 14:16:18 +0530

    converted the eventlistner to function onclickof submit function will be called

--- public/JS/app.js
+++ public/JS/app.js
@@ -7,33 +7,37 @@ const search=document.querySelector('input')
 const message1=document.querySelector('#message-1');
 const message2=document.querySelector('#message-2');
 
-weatherForm.addEventListener('submit',(e)=>{
+// weatherForm.addEventListener('submit',(e)=>{
 
-e.preventDefault();
-e.stopPropagation();
-message1.textContent="Loding....."
-message2.textContent=""
-console.log(search.value)
-fetch(`/weather?address=${search.value}`)
-   .then(function(res){res.json()
-         .then(function(data){
-         
-            // console.log(data)
-             if(data.error){
-                message1.textContent=data.error
+// e.preventDefault();
+const getData=()=>{
+   message1.textContent="Loading....."
+   message2.textContent=""
+   console.log(search.value)
+   fetch(`/weather?address=${search.value}`)
+      .then(function(res){res.json()
+            .then(function(data){
+            
+              console.log(data)
+                if(data.error){
+                   message1.textContent=data.error
+                   
+                }
+                 else
+                 {
+                   message1.textContent=data.forecast
+                   message2.textContent=data.address
+                  //  console.log(data.forecast)
+                  //  console.log(data.address)
+                  //  console.log(data.location)
+                 }
+            
                 
-             }
-              else
-              {
-                message1.textContent=data.forecast
-                message2.textContent=data.address
-               //  console.log(data.forecast)
-               //  console.log(data.address)
-               //  console.log(data.location)
-              }
-         
-             
-           })
-        
-    })
-})
+              })
+           
+       })
+       return false
+}
+
+   // return false;
+// })
